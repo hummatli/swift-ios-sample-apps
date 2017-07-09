b5abfe77cd70c0f86c5081af5110dac542eafcce
Sattar Hummatli <settarxan@gmail.com>
2017-07-09 16:27:03 +0400

    Played with variable with Variable.playground

---
+++ Variables/Variables.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Operators
+
+//Unary, Binary, Ternary
+
+var amICool = true
+
+amICool = !amICool
+
+var feelGoodAboutMyself = true
+
+feelGoodAboutMyself = amICool ? true : false
+
+var bankAccountBalance = 100
+
+var cashRegisterMessage = bankAccountBalance >= 50 ? "You just bought the item" : "You are broke as a joke"
